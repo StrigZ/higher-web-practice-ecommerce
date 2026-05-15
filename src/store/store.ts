403a4c90a52e04bc900dad6d2228b9d842326f23
StrigZ <mirgasan200@gmail.com>
2026-05-16 00:02:17 +0300

    Register all slices and apis

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,33 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
 
-import { cartSlice } from './features/cart/cart-slice';
 import { searchFiltersSlice } from './features/search-filter/search-filter-slice';
 import { userSlice } from './features/user/user-slice';
 
-export const rootReducer = combineSlices(
-  cartSlice,
-  searchFiltersSlice,
-  userSlice,
-);
+import { cartApi } from '@/api/cart-api';
+import { ordersApi } from '@/api/orders-api';
+import { productsApi } from '@/api/products-api';
+import { ratingsApi } from '@/api/ratings-api';
+import { usersApi } from '@/api/users-api';
 
+export const rootReducer = combineSlices(
+  userSlice,
+  searchFiltersSlice,
+  productsApi,
+  cartApi,
+  ordersApi,
+  ratingsApi,
+  usersApi,
+);
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      productsApi.middleware,
+      cartApi.middleware,
+      ordersApi.middleware,
+      ratingsApi.middleware,
+      usersApi.middleware,
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
