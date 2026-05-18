bf1478e36b3a32461072349fc759dc37989e18e2
StrigZ <mirgasan200@gmail.com>
2026-05-18 17:46:52 +0300

    Add categories popover to the header

--- src/components/layout/header/header-catalog.tsx
+++ src/components/layout/header/header-catalog.tsx
@@ -1,9 +1,40 @@
+import { Link } from 'react-router-dom';
+
 import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { productCategories } from '@/lib/constants';
 
 export function HeaderCatalog() {
   return (
-    <Button className="h-full cursor-pointer rounded-sm px-4 py-2 text-base font-bold">
-      Каталог
-    </Button>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-full cursor-pointer rounded-sm px-4 py-2 text-base font-bold">
+          Каталог
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverHeader>
+          <PopoverTitle className="text-2xl font-bold">Категории</PopoverTitle>
+        </PopoverHeader>
+        <ul className="flex flex-col gap-2">
+          {productCategories.map((category) => (
+            <li>
+              <Link
+                className="hover:text-secondary hover:bg-muted block w-full rounded-sm px-4 py-2 text-base"
+                to={`/?category=${category}`}
+              >
+                {category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </PopoverContent>
+    </Popover>
   );
 }
