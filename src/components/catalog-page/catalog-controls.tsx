b53b057d80126f4104d9ae5c04d68204a8fb752d
StrigZ <mirgasan200@gmail.com>
2026-05-21 06:11:09 +0300

    Add CatalogControls component for sorting and to change layout

---
+++ src/components/catalog-page/catalog-controls.tsx
@@ -0,0 +1,66 @@
+import { useSearchParams } from 'react-router-dom';
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { productSortingVariants } from '@/lib/constants';
+import type { ProductSort } from '@/types';
+
+const sortingVariantToLabelMap: Record<ProductSort, string> = {
+  newest: 'Сначала новые',
+  price_asc: 'Сначала дешевые',
+  price_desc: 'Сначала дорогие',
+  rating: 'Сначала популярные',
+};
+
+type Props = {
+  layoutStyle: 'сетка' | 'список';
+  updateLayoutStyle: (style: 'сетка' | 'список') => void;
+};
+export function CatalogControls({ layoutStyle, updateLayoutStyle }: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  return (
+    <div className="flex items-center gap-2">
+      <Select
+        value={searchParams.get('sortBy') ?? 'newest'}
+        onValueChange={(value) =>
+          setSearchParams((searchParams) => {
+            searchParams.set('sortBy', value);
+            return searchParams;
+          })
+        }
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Сортировка" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {productSortingVariants.map((variant) => (
+              <SelectItem value={variant}>
+                {sortingVariantToLabelMap[variant]}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <Select value={layoutStyle} onValueChange={updateLayoutStyle}>
+        <SelectTrigger>
+          <SelectValue placeholder="Отображение" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="сетка">Сеткой</SelectItem>
+            <SelectItem value="список">Списком</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
--- src/pages/catalog-page.tsx
+++ src/pages/catalog-page.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { Breadcrumbs } from '@/components/catalog-page/breadcrumbs';
+import { CatalogControls } from '@/components/catalog-page/catalog-controls';
 import { FilterSidebar } from '@/components/catalog-page/filter-sidebar/filter-sidebar';
 import { useProducts } from '@/hooks/use-products';
 
 export function CatalogPage() {
   const { isLoading, products } = useProducts();
   const [searchParams] = useSearchParams();
+
+  const [layoutStyle, setLayoutStyle] = useState<'сетка' | 'список'>('сетка');
 
   return (
     <div className="flex flex-col gap-5 py-8">
@@ -16,7 +20,10 @@ export function CatalogPage() {
         <div className="flex flex-1 flex-col gap-2">
           <header className="flex justify-between">
             <h1 className="text-3xl font-bold uppercase">Усы</h1>
-            <div>сортировка</div>
+            <CatalogControls
+              layoutStyle={layoutStyle}
+              updateLayoutStyle={(style) => setLayoutStyle(style)}
+            />
           </header>
           {isLoading ? (
             <div>loading</div>
