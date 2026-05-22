32bd230bdf379ea1bd3c37db752c5f2499e1ef03
StrigZ <mirgasan200@gmail.com>
2026-05-22 15:56:44 +0300

    Make breadcrumbs component reusable

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,14 @@
+export function Breadcrumbs({ values }: { values: (string | null)[] }) {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="text-muted-foreground flex items-center gap-2">
+        {values.filter(Boolean).map((value) => (
+          <li className="group">
+            <span className="group-first:hidden">/ </span>
+            {value}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
