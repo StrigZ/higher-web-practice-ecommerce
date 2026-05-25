89246778ec594cdc8119d0bfc7572e2a398d2431
StrigZ <mirgasan200@gmail.com>
2026-05-25 15:11:50 +0300

    Handle empty rating list

--- src/components/product-page/product-rating-card/product-rating-list.tsx
+++ src/components/product-page/product-rating-card/product-rating-list.tsx
@@ -8,29 +8,33 @@ export function ProductRatingList() {
 
   return (
     <ul className="flex flex-col gap-5">
-      {ratings.map(({ rating, userName }) => (
-        <li key={userName} className="flex flex-col gap-5">
-          <Separator />
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <ul className="flex items-center">
-                {Array(rating)
-                  .fill(<Star className="fill-primary size-5 stroke-none" />)
-                  .map((star, i) => (
-                    <li key={i}>{star}</li>
-                  ))}
-                {Array(5 - rating)
-                  .fill(<Star className="stroke-primary size-4 fill-none" />)
-                  .map((star, i) => (
-                    <li key={i}>{star}</li>
-                  ))}
-              </ul>
-              <p className="text-base font-bold">{rating}.0</p>
+      {ratings.length > 0 ? (
+        ratings.map(({ rating, userName }) => (
+          <li key={userName} className="flex flex-col gap-5">
+            <Separator />
+            <div className="flex items-center gap-4">
+              <div className="flex items-center gap-2">
+                <ul className="flex items-center">
+                  {Array(rating)
+                    .fill(<Star className="fill-primary size-5 stroke-none" />)
+                    .map((star, i) => (
+                      <li key={i}>{star}</li>
+                    ))}
+                  {Array(5 - rating)
+                    .fill(<Star className="stroke-primary size-4 fill-none" />)
+                    .map((star, i) => (
+                      <li key={i}>{star}</li>
+                    ))}
+                </ul>
+                <p className="text-base font-bold">{rating}.0</p>
+              </div>
+              <p className="text-base">{userName}</p>
             </div>
-            <p className="text-base">{userName}</p>
-          </div>
-        </li>
-      ))}
+          </li>
+        ))
+      ) : (
+        <p>Не отзывов</p>
+      )}
     </ul>
   );
 }
