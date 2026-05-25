9036bb7a7d20ed23ea1f3e11d72cc7e64831fac8
StrigZ <mirgasan200@gmail.com>
2026-05-25 18:11:49 +0300

    Hide cart summary if cart is empty

--- src/components/cart-page/cart-items.tsx
+++ src/components/cart-page/cart-items.tsx
@@ -1,21 +1,11 @@
 import { CartItem } from './cart-item';
 
-import { useGetCartQuery } from '@/api/cart-api';
-import { selectUserId } from '@/store/features/user/user-slice';
-import { useAppSelector } from '@/store/hooks';
+import type { CartItem as TCartItem } from '@/types';
 
-export function CartItems() {
-  const userId = useAppSelector(selectUserId);
-  const { data: cartItems, isLoading } = useGetCartQuery(
-    { userId: userId! },
-    { skip: !userId },
-  );
-
-  if (isLoading || !cartItems) return null;
-
-  return cartItems.length > 0 ? (
+export function CartItems({ items }: { items: TCartItem[] }) {
+  return items.length > 0 ? (
     <ul className="flex">
-      {cartItems.map(({ productId, id }) => (
+      {items.map(({ productId, id }) => (
         <li key={id}>
           <CartItem id={id} productId={productId} />
         </li>
--- src/pages/cart-page.tsx
+++ src/pages/cart-page.tsx
@@ -1,13 +1,26 @@
+import { useGetCartQuery } from '@/api/cart-api';
 import { CartItems } from '@/components/cart-page/cart-items';
 import { CartSummary } from '@/components/cart-page/cart-summary';
+import { selectUserId } from '@/store/features/user/user-slice';
+import { useAppSelector } from '@/store/hooks';
 
 export function CartPage() {
+  const userId = useAppSelector(selectUserId);
+  const { data: cartItems, isLoading } = useGetCartQuery(
+    { userId: userId! },
+    { skip: !userId },
+  );
+  if (isLoading || !cartItems) {
+    // TODO:add spinner
+    return <p>loading</p>;
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl font-bold">Корзина</h2>
       <div className="flex justify-between gap-5">
-        <CartItems />
-        <CartSummary />
+        <CartItems items={cartItems} />
+        {cartItems.length > 0 && <CartSummary />}
       </div>
     </div>
   );
