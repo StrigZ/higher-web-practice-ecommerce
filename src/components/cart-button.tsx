dc8f8055fbb5203760633173ec26bcb8fdd77044
StrigZ <mirgasan200@gmail.com>
2026-05-25 18:49:59 +0300

    Add missing prop

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -79,7 +79,7 @@ export function ShoppingCartButton({
   if (!userId) return null;
 
   return cartItem ? (
-    <div className="flex w-full flex-1 items-center justify-center gap-2">
+    <div className={cn('flex items-center justify-center gap-2', className)}>
       <Button
         className="h-10 w-10 rounded-sm text-sm"
         disabled={isMutationLoading}
