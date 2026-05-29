f329f1d55f1df31f0a9002ec6721973ca8c167f2
strigz <mirgasan200@gmail.com>
2026-05-29 18:26:22 +0300

    Update current route highlight logic in profile layout

--- src/components/layout/profile-layout/profile-layout.tsx
+++ src/components/layout/profile-layout/profile-layout.tsx
@@ -4,23 +4,30 @@ import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 
 const profileRoutes = [
-  { url: '/profile', label: 'Профиль' },
+  {
+    url: '/profile',
+    label: 'Мой профиль',
+    match: ['/profile', '/profile/edit'],
+  },
   { url: '/profile/orders', label: 'История заказов' },
   { url: '/profile/cart', label: 'Корзина' },
 ];
 
 export function ProfileLayout() {
   const location = useLocation();
+
   return (
     <div className="flex h-full items-start gap-5 py-10">
       <aside>
         <nav>
           <ul className="flex flex-col gap-2 pr-4">
-            {profileRoutes.map(({ url, label }) => (
+            {profileRoutes.map(({ url, label, match }) => (
               <li key={url}>
                 <Link
                   className={cn('block rounded-sm px-4 py-2 text-base', {
-                    'text-secondary bg-muted': location.pathname === url,
+                    'text-secondary bg-muted': (match ?? [url]).includes(
+                      location.pathname,
+                    ),
                   })}
                   to={url}
                 >
